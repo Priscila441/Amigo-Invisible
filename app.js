0ce16494b896f5d0631b402b8778a423a60ed00f
Priscila Cordoba <pricordoba441@gmail.com>
2025-03-08 15:11:00 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+// Fucionalidades:
+// Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+
+// Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
+
+// Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+
+// Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.
+
+// primero debemos seleccionar los elementos que vamos a utilizar del DOM
+let input = document.getElementById('amigo');
+let listaAmigos = [];
+
+function agregarAmigo(){
+    let amigo = input.value;
+    if (amigo == ''){}
+}
+
+function sortearAmigo(){
+
+}
