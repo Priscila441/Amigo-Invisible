55884abe854c7a27736e162a467c0054ca9f1cff
Priscila Cordoba <pricordoba441@gmail.com>
2025-03-10 08:30:39 -0300

    Funciones agregarAmigo y sortearAmigo ya funcionando

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-
-
-// Fucionalidades:
-// Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
-
-// Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
-
-// Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
-
-// Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.
-
-// primero debemos seleccionar los elementos que vamos a utilizar del DOM
 let input = document.getElementById('amigo');
+let listaAmigoUL = document.getElementById('listaAmigos');
+let resultado = document.getElementById('resultado'); 
 let listaAmigos = [];
 
+
 function agregarAmigo(){
-    let amigo = input.value;
-    if (amigo == ''){}
-}
+    let amigo = input.value.trim();
+    if (amigo === ''){
+        alert('Ingrese un nombre válido');
+    }
+    else{
+        listaAmigos.push(amigo);
+        let nuevoAmigo = document.createElement('li');
+        nuevoAmigo.textContent  = amigo;
+        listaAmigoUL.appendChild(nuevoAmigo);
+        input.value = '';
+    }
+}   
 
-function sortearAmigo(){
 
+function sortearAmigo(listaAmigos){
+
+    if(listaAmigos.length === 0){
+        alert('No hay amigos para sortear. Agregue al menos un amigo');
+    }
+    else{
+        let indiceAmigoSecreto = Math.floor(Math.random()*listaAmigos.length);
+    
+        let sorteo = document.createElement('p');
+        sorteo.textContent = `El amigo secreto es : ${listaAmigos[indiceAmigoSecreto]}`;
+        //resultado.innerHTML = '';
+        resultado.appendChild(sorteo);
+    }
+    
 }
